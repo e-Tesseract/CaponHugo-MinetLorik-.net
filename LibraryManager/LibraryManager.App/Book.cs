0a4894e5ccf706042860cb711c21887ff11c857f
Capon Hugo <capon.hugo@gmail.com>
2024-12-16 10:58:52 +0100

    add Book class (ETAPE 2)

---
+++ LibraryManager/LibraryManager.App/Book.cs
@@ -0,0 +1,11 @@
+﻿public class Book
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+
+    public Book(string name, string type)
+    {
+        Name = name;
+        Type = type;
+    }
+}
--- LibraryManager/LibraryManager.App/Program.cs
+++ LibraryManager/LibraryManager.App/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        List<Book> books =
+        [
+            new Book("Harry Potter", "Fantastique"),
+            new Book("Le tour du monde en 80 jours", "Aventure"),
+            new Book("Le seigneur des anneaux", "Fantastique"),
+        ];
+
+        foreach (Book book in books)
+        {
+            Console.WriteLine(book.Name + " - " + book.Type);
+        }
+    }
+}
