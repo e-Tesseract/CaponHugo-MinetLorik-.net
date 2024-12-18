d0711974faab6a43c450eaeef300e2fa57297f09
Capon Hugo <capon.hugo@gmail.com>
2024-12-18 10:46:22 +0100

    add api

---
+++ LibraryManager/LibraryManager.Hosting/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessObjects.Enum;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+
+
+namespace LibraryManager.Hosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly CatalogManager _catalogManager;
+
+        public BookController(CatalogManager catalogManager)
+        {
+            _catalogManager = catalogManager;
+        }
+
+        // GET: books
+        [HttpGet]
+        [Route("books")]
+
+        public IActionResult GetBooks()
+        {
+            var catalog = _catalogManager.GetCatalog();
+            return Ok(catalog);
+        }
+
+        // GET : book/{id}
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetBook(int id)
+        {
+            var book = _catalogManager.FindBook(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        // GET : books/{type}
+        [HttpGet]
+        [Route("{type}")]
+        public IActionResult GetBooksByType(TypeBook type)
+        {
+            var catalog = _catalogManager.GetCatalog(type);
+            if (catalog == null)
+            {
+                return NotFound();
+            }
+            return Ok(catalog);
+        }
+
+        // POST : book/add
+      
+
+    }
+}
--- LibraryManager/LibraryManager.Hosting/Program.cs
+++ LibraryManager/LibraryManager.Hosting/Program.cs
@@ -1,3 +1,10 @@
+using BusinessObjects.Entity;
+using System.IO;
+using DataAccessLayer.Contexts;
+using DataAccessLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+using Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add custom services
+//string path = Path.GetFullPath($"{Directory.GetCurrentDirectory()}\\library.db");
+string path = "C:\\Users\\capon\\Desktop\\iut\\BUT3\\.net\\CaponHugo-MinetLorik-.net\\LibraryManager\\DataAccessLayer\\library.db";
+builder.Services.AddScoped<IGenericRepository<Book>, GenericRepository<Book>>();
+builder.Services.AddScoped<IGenericRepository<Author>, GenericRepository<Author>>();
+builder.Services.AddScoped<IGenericRepository<Library>, GenericRepository<Library>>();
+builder.Services.AddScoped<ICatalogManager, CatalogManager>();
+builder.Services.AddDbContext<LibraryContext>(options =>
+  options.UseSqlite($"Data Source={path};")
+         .EnableSensitiveDataLogging(false)
+         .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning)))
+         );
+
 
 var app = builder.Build();
 
